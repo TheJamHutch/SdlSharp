5be0b7a94abdb042a54ee8fd3f4d7ea2851baf92
TheJamHutch <james.hutch@protonmail.com>
2021-05-23 00:23:57 +0100

    Made SdlSystem static

--- SdlSharpened/src/System/SdlSystem.cs
+++ SdlSharpened/src/System/SdlSystem.cs
@@ -3,12 +3,10 @@ using SDL2;
 
 namespace SdlSharpened
 {
-    // Make static?
-
     /// <summary>
     ///   Class represents the SDL system.
     /// </summary>
-    public class SdlSystem
+    public static class SdlSystem
     {
         // Used all over the place
         internal static IntPtr WindowPointer { get; set; }
@@ -17,17 +15,13 @@ namespace SdlSharpened
         /// <summary>
         ///   Creates an object representing the SDL system. Must be created before any other SdlSharpened objects.
         /// </summary>
-        public SdlSystem()
+        public static void Init()
         {
             SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
-            SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_JPG | SDL_image.IMG_InitFlags.IMG_INIT_PNG | SDL_image.IMG_InitFlags.IMG_INIT_TIF);
-            SDL_ttf.TTF_Init();
         }
 
-        ~SdlSystem()
+        public static void Quit()
         {
-            SDL_ttf.TTF_Init();
-            SDL_image.IMG_Quit();
             SDL.SDL_Quit();
         }
 
@@ -35,7 +29,7 @@ namespace SdlSharpened
         ///   Delays the SDL system's clock for the specified number of milliseconds.
         /// </summary>
         /// <param name="millis">The number of milliseconds to delay.</param>
-        public void Delay(int millis)
+        public static void Delay(int millis)
         {
             if (millis > 0)
             {
@@ -43,16 +37,11 @@ namespace SdlSharpened
             }
         }
 
-        public static uint Tickss() 
-        {
-            return SDL.SDL_GetTicks();
-        }
-
         /// <summary>
         ///   Gets the number of ticks since the SDL system was initialised.
         /// </summary>
         /// <returns>The tick count.</returns>
-        public uint Ticks()
+        public static uint Ticks()
         {
             return SDL.SDL_GetTicks();
         }
@@ -61,24 +50,18 @@ namespace SdlSharpened
         ///   Gets the major, minor, and patch version numbers of the currently installed version of SDL2. 
         /// </summary>
         /// <returns>A formatted string representing the SDL version.</returns>
-        public string Version()
+        public static string Version()
         {
             SDL.SDL_GetVersion(out var sdlVersion);
-            var imgVersion = SDL_image.IMG_Linked_Version();
-            //var ttfVersion = SDL_ttf.TTF_LinkedVersion();
-            //var mixVersion = SDL_mixer.MIX_Linked_Version();
 
-            return $"SDL2: {sdlVersion.major}.{sdlVersion.minor}.{sdlVersion.patch} \n" +
-                   $"SDL_image: {imgVersion.major}.{imgVersion.minor}.{imgVersion.patch} \n";
-                   //$"SDL_ttf: {ttfVersion.major}.{ttfVersion.minor}.{ttfVersion.patch} \n" +
-                   //$"SDL_mixer: {mixVersion.major}.{mixVersion.minor}.{mixVersion.patch}";
+            return $"SDL2: {sdlVersion.major}.{sdlVersion.minor}.{sdlVersion.patch} \n";
         }
 
         /// <summary>
         ///   Returns the base file path.
         /// </summary>
         /// <returns>The base file path string.</returns>
-        public string GetAppPath() 
+        public static string GetAppPath() 
         {
             return SDL.SDL_GetBasePath();
         }
@@ -87,7 +70,7 @@ namespace SdlSharpened
         ///   Gets the most recently flagged SDL error.
         /// </summary>
         /// <returns>The SDL error.</returns>
-        public string GetError()
+        public static string GetError()
         {
             return $"{SDL.SDL_GetError()}";
         }
@@ -95,7 +78,7 @@ namespace SdlSharpened
         /// <summary>
         ///   Clears SDL of errors.
         /// </summary>
-        public void ClearError() 
+        public static void ClearError() 
         {
             SDL.SDL_ClearError();
         }
@@ -104,7 +87,7 @@ namespace SdlSharpened
         ///   Sets the SDL error.
         /// </summary>
         /// <param name="error">The error to set.</param>
-        public void SetError(string error) 
+        public static void SetError(string error) 
         {
             SDL.SDL_SetError(error);
         }
